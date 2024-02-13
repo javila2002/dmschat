e33d51939472d09e25a2cd12c692c2e812f2e06b
javila2002 <josue_avila200@hotmail.com>
2024-02-12 20:38:20 -0600

    Chnagin Img name to Pics name directory

--- src/components/Sender.js
+++ src/components/Sender.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import bkgcSend from './img/send_image.png';
+// import bkgcSend from './Pics/send_image.png';
 
 
 
@@ -11,9 +11,9 @@ class Sender extends React.Component
         this.state = {
               messagetoSend : '',
               isDisabled : true,
-              iconSendenabled : `url('${ process.env.PUBLIC_URL + "/img/send_image.png" }')`,
-              iconSenddisabled : `url('${ process.env.PUBLIC_URL + "/img/send_image_dsb.png" }')`,
-              icontoSent:`url('${ process.env.PUBLIC_URL + "/img/send_image_dsb.png" }')`
+              iconSendenabled : `url('${ process.env.PUBLIC_URL + "/Pics/send_image.png" }')`,
+              iconSenddisabled : `url('${ process.env.PUBLIC_URL + "/Pics/send_image_dsb.png" }')`,
+              icontoSent:`url('${ process.env.PUBLIC_URL + "/Pics/send_image_dsb.png" }')`
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
